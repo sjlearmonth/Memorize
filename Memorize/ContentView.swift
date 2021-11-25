f71495f0eff46aae389a14043120184ecde5b9cd
Stephen J Learmonth <stephen.j.learmonth@gmail.com>
2021-11-25 18:10:10 +0000

    Finish lecture 1

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,13 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            RoundedRectangle(cornerRadius: 20.0)
+                .stroke(lineWidth: 3.0)
+            Text("Hello world")
+        }
+        .foregroundColor(.red)
+        .padding(.horizontal)
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+    
+    
 }
--- Memorize/MemorizeApp.swift
+++ Memorize/MemorizeApp.swift
@@ -15,3 +15,4 @@ struct MemorizeApp: App {
         }
     }
 }
+
