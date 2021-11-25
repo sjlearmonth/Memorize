851dab69a2dbca115f40d954648f5bd47b7ce373
Stephen J Learmonth <stephen.j.learmonth@gmail.com>
2021-11-25 16:34:49 +0000

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Stephen Learmonth on 25/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
