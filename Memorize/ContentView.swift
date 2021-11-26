7b1fe7013ad92e972ed289a09bc56ef48fa587c6
Stephen J Learmonth <stephen.j.learmonth@gmail.com>
2021-11-26 17:22:27 +0000

    Finish Lecture 2

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,14 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = ["🚂", "🚀", "🚁", "🚜", "🚌", "🚞", "🦼", "🚨",
+                  "🚒", "🚢", "🚑", "🚔", "🦯", "🏎", "✈️", "🚓",
+                  "🛥", "🚈", "🛸", "🛰", "💺", "🚡", "🏍", "🚠"]
+    @State var emojiCount = 24
+    
+    var body: some View {
+        VStack {
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))])
+                {
+                    ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                        CardView(content: emoji).aspectRatio(4/7, contentMode: .fill)
+                    }
+                }
+            }
+            .foregroundColor(.red)
+            Spacer()
+            HStack {
+                add
+                Spacer()
+                remove
+            }
+            .font(.largeTitle)
+            .padding(.horizontal)
+        }
+        .padding(.horizontal)
+    }
+    
+    var remove: some View {
+        Button {
+            if emojiCount > 1 {
+                emojiCount -= 1
+            }
+        } label: {
+            Image(systemName: "minus.circle")
+        }
+    }
+    
+    var add: some View {
+        Button {
+            if emojiCount < emojis.count {
+                emojiCount += 1
+            }
+        } label: {
+            Image(systemName: "plus.circle")
+        }
+    }
+}
+
+
+struct CardView: View {
+    var content: String
+    @State var isFaceUp: Bool = true
+    
     var body: some View {
         ZStack {
-            RoundedRectangle(cornerRadius: 20.0)
-                .stroke(lineWidth: 3.0)
-            Text("Hello world")
+            let shape = RoundedRectangle(cornerRadius: 20.0)
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.strokeBorder(lineWidth: 3.0)
+                Text(content).font(.largeTitle)
+            } else {
+                shape.fill()
+            }
         }
-        .foregroundColor(.red)
-        .padding(.horizontal)
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
     }
 }
 
@@ -34,16 +94,9 @@ struct ContentView: View {
 
 
 
-
-
-
-
-
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().preferredColorScheme(.dark).previewDisplayName("Dark Mode")
+        ContentView().preferredColorScheme(.light).previewDisplayName("Light Mode")
     }
-    
-    
 }
